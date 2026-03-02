4bb9651b39079ba7095d777a170f22a211817c84
rrtucci <tucci@ar-tiste.com>
2026-03-01 21:14:43 -0500

    (1 =

--- plotting.py
+++ plotting.py
@@ -2,64 +2,100 @@ import matplotlib.pyplot as plt
 from rg_functions import *
 
 
-def plot_function(fun,
-                  xlabel="x",
-                  ylabel="y",
-                  caption="",
-                  x_min=-5,
-                  x_max=5,
-                  num_points=100):
+def plot_functions(funs,
+                   fun_names=None,
+                   xlabel="x",
+                   ylabel="y",
+                   caption="",
+                   x_min=-5,
+                   x_max=5,
+                   num_points=100):
     """
-    Plots the function fun(x) over the interval [x_min, x_max].
+    Plots several functions fun(x) over the interval [x_min, x_max].
 
     Parameters:
-        fun         : callable, function of one variable
+        funs         : list of functions of one variable
         x_min       : float, left endpoint
         x_max       : float, right endpoint
         num_points  : int, number of sample points
     """
+    # plt.close('all')
     x = np.linspace(x_min, x_max, num_points)
-    y = fun(x)
+    fig, ax = plt.subplots()  # single frame / single axes
 
-    plt.figure()
-    plt.plot(x, y)
+    for i, fun in enumerate(funs):
+        y = fun(x)
+        if not fun_names:
+            label = f"fun_{i + 1}(x)"
+        else:
+            label = fun_names[i]
+        ax.plot(x, y, label=label)
+
+    # Add x = y in red
+    ax.plot(x, x, 'r-', linewidth=2, label="x = y")
+
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(caption)
+    plt.legend()
     plt.grid(True)
-    # Plot x = y in red
-    plt.plot(x, x, 'r-', label="x = y")
     plt.show()
 
 
 if __name__ == "__main__":
-    def main1():
-        plot_function(One_Dim_Ising.K_prime,
-                      xlabel="K",
-                      ylabel="K'(K)",
-                      caption="plot of K'(K) for 1-dim ising",
-                      x_min=0,
-                      x_max=2,
-                      num_points=100)
+    def main0():
+        funs = [
+            lambda x: np.sin(x),
+            lambda x: np.cos(x),
+            lambda x: 0.5 * x
+        ]
 
-        plot_function(Two_Dim_Ising.K_prime,
-                      xlabel="K",
-                      ylabel="K'(K)",
-                      caption="plot of K'(K) for 2-dim ising",
-                      x_min=0,
-                      x_max=2,
-                      num_points=100)
+        plot_functions(funs)
+
+
+    def main1():
+        plot_functions([One_Dim_Ising.K_prime],
+                       xlabel="K",
+                       ylabel="K'(K)",
+                       caption="plot of K'(K) for 1-dim ising",
+                       x_min=0,
+                       x_max=2,
+                       num_points=100)
+
+        plot_functions([Two_Dim_Ising.K_prime],
+                       xlabel="K",
+                       ylabel="K'(K)",
+                       caption="plot of K'(K) for 2-dim ising",
+                       x_min=0,
+                       x_max=2,
+                       num_points=100)
 
 
     def main2():
-        plot_function(Two_Dim_Ising.zeta,
-                      xlabel="K",
-                      ylabel="zeta(K)",
-                      caption="plot of zeta(K) for 2-dim ising",
-                      x_min=0,
-                      x_max=2,
-                      num_points=100)
+        plot_functions([Two_Dim_Ising.zeta],
+                       xlabel="K",
+                       ylabel="zeta(K)",
+                       caption="plot of zeta(K) for 2-dim ising",
+                       x_min=0,
+                       x_max=2,
+                       num_points=100)
 
 
-    main1()
+    def main3(mean_spin):
+        mom = Two_Dim_Ising_Dbnet(mean_spin)
+        funs = [mom.K_prime_minus, mom.K_prime_av, mom.K_prime_plus]
+        fun_names = ["K' minus", "K' av", "K' plus"]
+
+        plot_functions(funs,
+                       fun_names=fun_names,
+                       xlabel="K",
+                       ylabel="K'(K)",
+                       caption="plot of K'(K) for 2-dim ising-dbnet",
+                       x_min=0,
+                       x_max=2,
+                       num_points=100)
+
+
+    # main1()
     # main2()
+    main3(1)
--- rg_functions.py
+++ rg_functions.py
@@ -23,3 +23,36 @@ class Two_Dim_Ising:
         f = Two_Dim_Ising.f(K)
         fprime = Two_Dim_Ising.f(Kprime)
         return .5 * np.log(f * fprime)
+
+
+class Two_Dim_Ising_Dbnet:
+
+    def __init__(self, mean_spin):
+        self.mean_spin = mean_spin
+
+    def K_prime_av(self, K):
+        cosh_0 = np.cosh(2 * K)
+        cosh_m = np.cosh(K * (self.mean_spin - 2))
+        cosh_p = np.cosh(K * (self.mean_spin + 2))
+        return K + 2 * .25 * np.log(cosh_0 ** 2 / (cosh_m * cosh_p))
+
+    def K_prime_plus(self, K):
+        cosh_0 = np.cosh(2 * K)
+        cosh_p = np.cosh(K * (self.mean_spin + 2))
+        return K * (1 + .5 * self.mean_spin) + 2 * .5 * np.log(cosh_0 / cosh_p)
+
+    def K_prime_minus(self, K):
+        cosh_0 = np.cosh(2 * K)
+        cosh_m = np.cosh(K * (self.mean_spin - 2))
+        return K * (1 - .5 * self.mean_spin) + 2 * .5 * np.log(cosh_0 / cosh_m)
+
+
+if __name__ == "__main__":
+    def main1():
+        K = 1
+        for mean_spin in [-1, 1, -.3, .3, 0]:
+            K_prime = Two_Dim_Ising_Dbnet(mean_spin).K_prime_av(K)
+            print(mean_spin, K_prime)
+
+
+    main1()
